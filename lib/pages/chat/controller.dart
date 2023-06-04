893d41b6e62fa5e8fd7c5be050e0ffa13fc2b1f8
neIsonp <nelsonponte37@gmail.com>
2023-06-04 10:37:13 +0000

    getting data from router

--- lib/pages/chat/controller.dart
+++ lib/pages/chat/controller.dart
@@ -1,19 +1,32 @@
+import 'package:firebase_chat/common/store/store.dart';
 import 'package:firebase_chat/pages/chat/state.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
-import '../../common/routes/names.dart';
-import '../../common/store/config.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatController extends GetxController {
   final state = ChatState();
   ChatController();
+  var doc_id;
 
-  changePage(int index) async {
-    state.index.value = index;
+  final textController = TextEditingController();
+  ScrollController msgController = ScrollController();
+  FocusNode contentNode = FocusNode();
+  final user_id = UserStore.to.token;
+  final db = FirebaseFirestore.instance;
+
+  @override
+  void onInit() {
+    super.onInit();
+    var data = Get.parameters;
+    doc_id = data['doc_id'] ?? "";
+    state.to_uid.value = doc_id;
+    state.to_name.value = data['to_name'] ?? "";
+    state.to_avatar.value = data['to_avatar'] ?? "";
   }
 
-  handleSignIn() async {
-    await ConfigStore.to.saveAlreadyOpen();
-    Get.offAndToNamed(AppRoutes.SIGN_IN);
+  sendMessage() {
+    String sendContent = textController.text;
+    final content = 
   }
 }
--- lib/pages/chat/state.dart
+++ lib/pages/chat/state.dart
@@ -1,5 +1,10 @@
+import 'package:firebase_chat/common/entities/msgcontent.dart';
 import 'package:get/get.dart';
 
 class ChatState {
-  var index = 0.obs;
+  RxList<Msgcontent> msgContentList = <Msgcontent>[].obs;
+  var to_uid = "".obs;
+  var to_name = "".obs;
+  var to_avatar = "".obs;
+  var to_location = "Localização desconhecida".obs;
 }
