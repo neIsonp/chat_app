f229089cc7ea69d4137d5c93ecc3216fce0be9ac
neIsonp <nelsonponte37@gmail.com>
2023-06-04 11:48:36 +0000

    created widget to show messages in right

---
+++ lib/pages/message/chat/widgets/chat_right_item.dart
@@ -0,0 +1,42 @@
+import 'package:firebase_chat/common/entities/entities.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+
+Widget ChatRightItem(Msgcontent item) {
+  return Container(
+    padding: EdgeInsets.only(top: 10.w, left: 15.w, right: 15.w, bottom: 10.w),
+    child: Row(
+      children: [
+        ConstrainedBox(
+          constraints: BoxConstraints(maxWidth: 230.w, minHeight: 40.2),
+          child: Container(
+            margin: EdgeInsets.only(right: 10.w, top: 0.w),
+            padding: EdgeInsets.only(
+              top: 10.w,
+              left: 10.w,
+              right: 10.w,
+            ),
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              borderRadius: BorderRadius.all(
+                Radius.circular(10.w),
+              ),
+            ),
+            child: item.type == "text"
+                ? Text("${item.content}")
+                : ConstrainedBox(
+                    constraints: BoxConstraints(maxWidth: 90.w),
+                    child: GestureDetector(
+                      onTap: () {},
+                      child: CachedNetworkImage(
+                        imageUrl: "${item.content}",
+                      ),
+                    ),
+                  ),
+          ),
+        )
+      ],
+    ),
+  );
+}
